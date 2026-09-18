38d88558f30eafed912f785a29a2daa74cf67552
Rohit Rautela <rohitrautela3231@gmail.com>
2026-09-18 18:59:22 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    vector<string>v;
+
+    string encode(vector<string>& strs) {
+        string ans="";
+        for(auto s:strs){
+            v.push_back(s)
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        
+        return v;
+    }
+};
